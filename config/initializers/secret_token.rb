b4fe28fb66a348e8efbe6d0229486cfe4b08844c
James Oladipo <jloladipo@gmail.com>
2013-11-11 17:58:43 -0500

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Texttle::Application.config.secret_key_base = '0f5d2d932df73f75e24aa7f5e5515b2d604fd10a34fe0a512fdf54ab1203d13bef66a7c7f872bf77d9458506faa7d19bca49abedaf26c0e3145948782ff577c6'
